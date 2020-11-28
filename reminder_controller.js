e75a099765bf9657af3ed5a1c0cd0466d492edd1
Ultrarceus <62914080+Ultrarceus@users.noreply.github.com>
2020-11-27 16:56:11 -0800

    Add files via upload

--- reminder_controller.js
+++ reminder_controller.js
@@ -1,13 +1,15 @@
 let database = require("../database");
+const fetch = require('node-fetch')
 
 let remindersController = {
   list: (req, res, next) => {
-    res.locals.reminders = "/reminders"
+    res.locals.page = "reminders"
     res.render('reminder/index', { reminders: database.cindy.reminders })
   },
 
   new: (req, res) => {
     res.locals.new = "/new"
+    
     res.render('reminder/create')
   },
 
@@ -24,14 +26,24 @@ let remindersController = {
   },
 
   create: (req, res) => {
-    let reminder = {
-      id: database.cindy.reminders.length + 1,
-      title: req.body.title,
-      description: req.body.description,
-      completed: false
-    }
-    database.cindy.reminders.push(reminder);
-    res.redirect('/reminders');
+    const clientid = "fzEDnXbeWLPJTiUvAU-9uGrABjwjEQmDTd5gQIS3dAI"
+    const query = req.body.coverInput
+    const url = `https://api.unsplash.com/search/photos?client_id=${clientid}&query=${query}`
+    fetch(url)
+    .then(data => data.json())
+    .then(newData => {
+      const imageFromUnsplash = newData.results[0].urls.regular
+      let reminder = {
+        id: database.cindy.reminders.length + 1,
+        title: req.body.title,
+        description: req.body.description,
+        completed: false,
+        coverPhoto: imageFromUnsplash
+      }
+      .catch(err => console.log(err)) 
+      database.cindy.reminders.push(reminder);
+      res.redirect('/reminders');
+    })
   },
 
   edit: (req, res) => {
